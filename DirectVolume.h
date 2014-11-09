3c17a6ae35f1613ff781835afd7256a9fc460ce3
Agontuk <rifat662@gmail.com>
2014-11-09 23:59:22 +0600

    Revert "Support non-sequential partition index"
    Causes vold to use random major,minor block number
    
    This reverts commit b31f33b5e41bf295705b8f0c1582c6277e8fec64.
    
    Change-Id: I8015289d49bb0b982d39d0b976b2b8f66801c0dd

--- DirectVolume.h
+++ DirectVolume.h
@@ -50,7 +50,7 @@ protected:
     int            mOrigDiskMinor;
     int            mOrigPartMinors[MAX_PARTITIONS];
     int            mDiskNumParts;
-    int            mPendingPartCount;
+    unsigned int   mPendingPartMap;
     int            mIsDecrypted;
 
 public:
